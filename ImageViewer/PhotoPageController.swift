3589433c991771bc1fdbaf914f73ae990904d61b
Jeff Ripke <jripke74@gmail.com>
2018-01-17 09:52:24 -0800

    added PhotoViewerController and PhotoPageController

---
+++ ImageViewer/PhotoPageController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoPageController.swift
+//  ImageViewer
+//
+//  Created by Jeff Ripke on 1/17/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PhotoPageController: UIPageViewController {
+    
+}
---
+++ ImageViewer/PhotoViewerController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoViewerController.swift
+//  ImageViewer
+//
+//  Created by Jeff Ripke on 1/17/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewerController: UIViewController {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
